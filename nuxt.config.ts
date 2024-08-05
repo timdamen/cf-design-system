38b154e1d233cb1d515e54408f9a1d48f4e167f3
Tim Damen <tim.damen80@gmail.com>
2024-08-05 15:46:49 +0200

    chore: update tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
   devtools: { enabled: true },
   compatibilityDate: "2024-08-05",
 });
